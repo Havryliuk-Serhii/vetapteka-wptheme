76b9ad7b06d99c7ce357eede9047471665cc0c22
Havryliuk-Serhii <48591061+Havryliuk-Serhii@users.noreply.github.com>
2020-02-05 13:18:10 +0200

    add files: comments.php, search.php, 404.php

---
+++ 404.php
@@ -0,0 +1,6 @@
+<?php get_header(); ?>
+	<div class="error-page">
+        <h1 class="main-title">404</h1>
+        <h2 class="main-subtitle"><?php esc_html_e( 'Упсс... Нічого не знайдено.', 'vetapteka' ); ?></h2>
+    </div>            
+<?php get_footer(); ?>
