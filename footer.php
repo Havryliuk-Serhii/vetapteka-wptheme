c17c0ace9a3de6a25f252dffbfdec934b28c8337
Havryliuk-Serhii <48591061+Havryliuk-Serhii@users.noreply.github.com>
2020-02-09 19:54:20 +0200

    delete junk scripts in footer.php

--- footer.php
+++ footer.php
@@ -13,37 +13,18 @@
     </div>          
   </main>
 	<footer class="footer">
-        <div class="container">
-			<div class="social-icon">
-			<?php if(!dynamic_sidebar( 'icons_footer' )): ?>
-    <span class="footer-text">Место для иконок</span>
-<?php endif; ?>    
-					
-		    </div>
-            <div class="copyright">
-					&copy; Ветеринарна аптека "Чотири лапи"  2016
-            </div>
-             			
-        </div>
-    </footer>
-
-<?php wp_footer(); ?>
-
-       
-        <script type="text/javascript">
-          $(document).ready(function() {
-            $("#carousel").waterwheelCarousel({
-              // include options like this:
-              // (use quotes only for string values, and no trailing comma after last option)
-              // option: value,
-              // option: value
-            });
-          });
-        </script>
-         <script>
-              new WOW().init();
-              </script>
-
+    <div class="container">
+		  <div class="social-icon">
+			  <?php if(!dynamic_sidebar( 'icons_footer' )): ?>
+        <span class="footer-text">Место для иконок</span>
+        <?php endif; ?>    
+			</div>
+      <div class="copyright">
+				&copy; Ветеринарна аптека "Чотири лапи"  2016
+      </div>             			
+    </div>
+  </footer>
+<?php wp_footer(); ?>       
 </body>
 </html>
 
